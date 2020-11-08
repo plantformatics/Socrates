44eb07e4b302f8cfc1262a3183b07994975d3834
MisterPotatoHead <marand@wisc.edu>
2020-11-08 12:35:17 -0500

    updated vignette

--- R/coaccessibility.R
+++ R/coaccessibility.R
@@ -3,19 +3,227 @@
 ###################################################################################################
 #' coAccess
 #'
-#' This function loads the sparse matrix and meta data and returns a list - counts & meta
+#' This function identifies co-accessible ACRs within clusters or across the entire data set
 #'
-#' @importFrom Matrix sparseMatrix
-#' @importFrom Matrix colSums
-#' @importFrom cicero
+#' @import Matrix
+#' @import cicero
+#' @import parallel
 #'
-#' @param input Path to sparse cell x peak text file in triplet format. Input data can be gzipped.
-#' Required.
-#' @param meta Path to meta data for cells in sparse cell x peak data set in tsv format. Headers
-#' and barcode/cellID rownames are required. Meta data can be gzipped. Providing meta data is
-#' highly recommended, but not required.
+#' @param obj Socrates object. Required.
+#' @param genome path to genome file. See 'inst/extdata/genome' for an example. Required.
+#' @param ann gene annotation file in BED format. See 'inst/extdata/gene_annotation.bed' for an
+#' example. Required.
+#' @param byGroup logical. Whether to run co-accessibility tests for each factor in 'groupID'.
+#' Defaults to False.
+#' @param groupID character. Column name in obj$meta for partitioning during co-accessibility
+#' analysis. Defaults to "LouvainClusters".
+#' @param eFDR logical. Whether or not to use empirical FDR to filter co-accessible ACRs. Defaults
+#' to FALSE. Setting eFDR to 'TRUE' doubles memory usage.
+#' @param fdr_thresh float. fdr threshold when using empirical FDR. Defaults to 0.05.
+#' @param win_size numeric. Genomic distance to constrain co-accessible ACRs. Defaults to 500000.
+#' @param sample_num numeric. Number of random windows to sample for determining distance_parameter
+#'  during 'run_cicero'. See cicero for more details.
+#' @param k numeric. Number of nearest neighbors for aggregating cells. Defaults to 30.
+#' @param conn_slotName character. Slot name for depositing co-accessible ACRs. Defaults to coACRs.
+#' @param nthreads numeric. Number of threads to use for estimating co-ACRs for each cluster or
+#' celltype. Defaults to 1.
 #' @param verbose logical. Defaults to FALSE.
 #'
 #' @rdname coAccess
 #' @export
 #'
+coAccess <- function(obj,
+                     genome=NULL,
+                     ann=NULL,
+                     byGroup=FALSE,
+                     groupID="LouvainClusters",
+                     eFDR=FALSE,
+                     fdr_thresh=0.05,
+                     win_size=500000,
+                     sample_num=100,
+                     k=30,
+                     conn_slotName="coACRs",
+                     nthreads=1,
+                     verbose=FALSE){
+
+    # hidden functions
+    .loadMeta <- function(cds, meta, clusterID){
+
+        # svd and raw
+        ids <- colnames(cds)
+        meta <- jac[ids,]
+        meta$UMAP1 <- jac$umap1
+        meta$UMAP2 <- jac$umap2
+        ids.2 <- rownames(meta)
+        cds <- cds[,colnames(cds) %in% ids.2]
+        umap.d <- t(meta[,c("UMAP1","UMAP2")])
+
+        # UMAP output
+        cds@reducedDimA <- umap.d
+        cds@reducedDimS <- umap.d
+        cds@dim_reduce_type <- "UMAP"
+        pData(cds)$Cluster <- as.factor(meta[,clusterID])
+
+        # return data
+        return(cds)
+    }
+
+    # check assumptions
+    if(is.null(genome)){
+        stop("! argument 'genome' is required, exiting...")
+    }
+    if(is.null(ann)){
+        stop("! argument 'ann' is required, exiting...")
+    }
+    if(byGroup==T){
+        if(is.null(groupID)){
+            stop(" ! argument groupID is required when 'byGroup' is set to TRUE, exiting...")
+        }
+        if(!groupID %in% colnames(obj$meta)){
+            stop(" ! argument groupID must be a column name in the 'meta' slot of obj, exiting...")
+        }
+    }
+
+    # create cicero objects and process
+    obs <- as.data.frame(summary(obj$counts))
+    obs$i <- factor(rownames(obj$counts)[obs$i])
+    obs$j <- factor(colnames(obj$counts)[obs$j])
+    cds <- make_atac_cds(obs, binarize=T)
+    cds <- cds[,colnames(cds) %in% rownames(obj$meta)]
+    pData(cds) <- meta[colnames(exprs(cds)),]
+    cds <- cds[Matrix::rowSums(exprs(cds))>0,]
+    cds <- cds[,Matrix::colSums(exprs(cds))>0]
+    cds <- detectGenes(cds)
+    cds <- estimateSizeFactors(cds)
+    cds <- .loadMeta(cds, obj$meta)
+
+    # run per cluster or not
+    if(eFDR==FALSE){
+
+        if(byGroup==TRUE){
+
+            # get cluster ids
+            clusts <- unique(obj$meta[,groupID])
+
+            # run in parallel
+            outs <- mclapply(clusts, function(z){
+                cluster.ids <- rownames(subset(obj$meta, obj$meta[,groupID]==z))
+                sub.cds <- cds[,colnames(exprs(cds)) %in% cluster.ids]
+                sub.cds <- sub.cds[Matrix::rowSums(exprs(sub.cds))>0,]
+                sub.cds <- sub.cds[,Matrix::colSums(exprs(sub.cds))>0]
+                sub.conns <- runCicero(sub.cds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+                sub.conns$group <- z
+            }, mc.cores=nthreads)
+
+            # join into a single df
+            outs <- as.data.frame(do.call(rbind, outs))
+            obj[[conn_slotName]] <- outs
+
+        }else{
+            obj[[conn_slotName]] <- runCicero(cds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+        }
+
+    }else{
+
+        # shuffle
+        shuf <- obs
+        shuf$i <- shuf$i[sample(length(shuf$i))]
+        shuf$j <- shuf$j[sample(length(shuf$j))]
+        shufcds <- make_atac_cds(shuf, binarize=T)
+
+        # add metadata and filter
+        shufcds <- shufcds[,colnames(shufcds) %in% rownames(obj$meta)]
+        pData(shufcds) <- meta[colnames(exprs(shufcds)),]
+        shufcds <- shufcds[Matrix::rowSums(exprs(shufcds))>0,]
+        shufcds <- shufcds[,Matrix::colSums(exprs(shufcds))>0]
+
+        # process
+        shufcds <- detectGenes(shufcds)
+        shufcds <- estimateSizeFactors(shufcds)
+
+        # update cluster/UMAP info
+        shufcds <- .loadMeta(shufcds, obj$meta)
+
+        # run cicero by cluster or no
+        if(byGroup==TRUE){
+
+            # get cluster ids
+            clusts <- unique(obj$meta[,groupID])
+
+            # run in parallel
+            outs <- mclapply(clusts, function(z){
+                cluster.ids <- rownames(subset(obj$meta, obj$meta[,groupID]==z))
+                sub.cds <- cds[,colnames(exprs(cds)) %in% cluster.ids]
+                sub.cds <- sub.cds[Matrix::rowSums(exprs(sub.cds))>0,]
+                sub.cds <- sub.cds[,Matrix::colSums(exprs(sub.cds))>0]
+                sub.shufcds <- shufcds[,colnames(exprs(shufcds)) %in% cluster.ids]
+                sub.shufcds <- sub.shufcds[Matrix::rowSums(exprs(sub.shufcds))>0,]
+                sub.shufcds <- sub.shufcds[,Matrix::colSums(exprs(sub.shufcds))>0]
+
+                # run
+                sub.conns <- runCicero(sub.cds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+                sub.shufconns <- runCicero(sub.shufcds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+
+                # do empirical FDR
+                sub.conns$group <- z
+
+
+            }, mc.cores=nthreads)
+
+            # join into a single df
+            outs <- as.data.frame(do.call(rbind, outs))
+            obj[[conn_slotName]] <- outs
+
+        }else{
+            conns1 <- runCicero(cds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+            conns2 <- runCicero(shufcds, genome=genome, k=k, win=win_size, sample_num=sample_num)
+        }
+
+    }
+
+}
+
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+#' runCicero
+#'
+#' This function is a wrapper for several cicero commands
+#'
+#' @import cicero
+#'
+#' @param cds cicero object. Required.
+#' @param genome path to genome file. See 'inst/extdata/genome' for an example. Required.
+#' @param k numeric. Number of nearest neighbors for binning cells. See cicero for more details.
+#' Defaults to 30.
+#' @param win numeric. Window size for constraining co-accessible ACRs. Defaults to 500000.
+#' @param sample_num numeric. Number of random windows to sample for determining distance_parameter
+#'  during 'run_cicero'. See cicero for more details.
+#' @param silent logical. Defaults to TRUE.
+#'
+#' @rdname runCicero
+#'
+runCicero <- function(cds,
+                      genome=NULL,
+                      k=30,
+                      win=500000,
+                      sample_num=100,
+                      silent=TRUE){
+
+    # get UMAP coordinates
+    umap_coords <- t(reducedDimA(cds))
+    umap_coords <- umap_coords[colnames(cds),]
+    rownames(umap_coords) <- colnames(exprs(cds))
+    cicero_cds <- make_cicero_cds(cds, reduced_coordinates=umap_coords, k=k)
+
+    # run cicero (connections)
+    conns <- run_cicero(cicero_cds, genome, window=500000, sample_num=100, silent=silent)
+
+    # return
+    return(conns)
+
+}
+
+
