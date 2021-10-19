c351dd49d514df7691d3d5ebfc5fa18f4d20229b
plantformatics <marand@uga.edu>
2021-10-19 14:32:07 -0400

    updated findCells

--- R/quality_control.R
+++ R/quality_control.R
@@ -444,14 +444,10 @@ findCells <- function(obj,
         x$zscore[is.infinite(x$zscore) & x$zscore < 0] <- min(x$zscore[is.finite(x$zscore)])
         x$zscore[is.infinite(x$zscore) & x$zscore > 0] <- max(x$zscore[is.finite(x$zscore)])
         x <- x[order(x$zscore, decreasing=T),]
-	prop.z.min <- max(x$prop[x$zscore < -1*z.thresh])
-	if(is.null(prop.z.min)){
-	    thresh <- min.freq
-	}else if(is.na(prop.z.min)){
-	    thresh <- min.freq
-	}else if(prop.z.min > min.freq){
-	    thresh <- prop.z.min
-	}else{
+	score <- any(x$zscore < -1*z.thresh)
+	if(score){
+		prop.z.min <- max(x$prop[x$zscore < -1*z.thresh])
+	else{
             thresh <- min.freq
         }
         x <- x[order(x$total, decreasing=T),]
@@ -498,14 +494,10 @@ findCells <- function(obj,
         x$zscore[is.infinite(x$zscore) & x$zscore < 0] <- min(x$zscore[is.finite(x$zscore)])
         x$zscore[is.infinite(x$zscore) & x$zscore > 0] <- max(x$zscore[is.finite(x$zscore)])
         x <- x[order(x$zscore, decreasing=T),]
-	prop.z.min <- max(x$prop[x$zscore < -1*z.thresh])
-        if(is.null(prop.z.min)){
-            thresh <- min.freq
-        }else if(is.na(prop.z.min)){
-            thresh <- min.freq
-        }else if(prop.z.min > min.freq){
-            thresh <- prop.z.min
-        }else{
+	score <- any(x$zscore < -1*z.thresh)
+        if(score){
+            prop.z.min <- max(x$prop[x$zscore < -1*z.thresh])
+        else{
             thresh <- min.freq
         }
         x <- x[order(x$total, decreasing=T),]
