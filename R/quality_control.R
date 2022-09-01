d06c7d2351776dd3996d34643e10ed21f6dd2ec5
Alexandre Marand <marand@uga.edu>
2022-09-01 09:42:12 -0400

    Update quality_control.R

--- R/quality_control.R
+++ R/quality_control.R
@@ -93,7 +93,7 @@ loadBEDandGenomeData <- function(bed, ann, sizes, attribute="Parent", verbose=T,
         
         if(verbose){message(" - converting fragment file to single-bp resolution Tn5 insertions sites ...")}
         start.coordinates <- data.frame(V1=a$V1, V2=(a$V2), V3=(a$V2+1), V4=a$V4, V5="+")
-        end.coordinates <- data.frame(V1=a$V1, V2=(a$V3-1), V3=a$V2, V4=a$V4, V5="-")
+        end.coordinates <- data.frame(V1=a$V1, V2=(a$V3-1), V3=a$V3, V4=a$V4, V5="-")
         all.coordinates <- rbind(start.coordinates, end.coordinates)
         all.coordinates <- all.coordinates[order(all.coordinates$V1, all.coordinates$V2, decreasing=F),]
         a <- all.coordinates[!duplicated(all.coordinates),]
